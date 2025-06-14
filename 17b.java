271f6a6a823fb2a5ca33a275841bc8b650c91e11
1tiagofr <tiagfox06@gmail.com>
2025-06-14 11:05:17 -0300

    17b.java

---
+++ 17b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumaParesSinIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número para sumar los pares hasta ese valor: ");
+        int num = sc.nextInt();
+
+        // El mayor número par <= num
+        int maxPar = (num % 2 == 0) ? num : num - 1;
+        int cantidadPares = maxPar / 2;
+
+        // Suma = 2 + 4 + 6 + ... + maxPar = 2*(1 + 2 + ... + cantidadPares)
+        int suma = 2 * (cantidadPares * (cantidadPares + 1) / 2);
+
+        System.out.println("Suma de los valores pares hasta " + num + " es: " + suma);
+        sc.close();
+    }
+}
