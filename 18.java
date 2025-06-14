9942fb4f4ad7ad3737d4d7b0fff83e4c8fabf3ef
1tiagofr <tiagfox06@gmail.com>
2025-06-14 11:16:53 -0300

    18.java

---
+++ 18.java
@@ -0,0 +1,11 @@
+public class TodasTablas {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 10; num++) {
+            System.out.println("Tabla del " + num + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+            System.out.println(); // Línea en blanco entre tablas
+        }
+    }
+}
