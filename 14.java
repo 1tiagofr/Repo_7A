6e084adcb3002eaf5063a9f58ef00417ddd5ad67
1tiagofr <tiagfox06@gmail.com>
2025-06-14 11:03:27 -0300

    14.java

---
+++ 14.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PuestoTorneoSwitch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir el puesto del torneo
+        System.out.print("Ingrese el puesto que obtuvo en el torneo (número entero): ");
+        int puesto = scanner.nextInt();
+
+        switch (puesto) {
+            case 1:
+                System.out.println("El primero obtiene la medalla de oro.");
+                break;
+            case 2:
+                System.out.println("El segundo obtiene la medalla de plata.");
+                break;
+            case 3:
+                System.out.println("El tercero obtiene la medalla de bronce.");
+                break;
+            default:
+                System.out.println("Siga participando.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
