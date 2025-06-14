0dfbcd747d042ba201b443353a8b7fc0a9a740cf
1tiagofr <tiagfox06@gmail.com>
2025-06-14 11:04:39 -0300

    16.java

---
+++ 16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número para mostrar su tabla de multiplicar: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+        sc.close();
+    }
+}
