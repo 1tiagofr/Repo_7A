3ab90802445c1bdbcd4d44bd37efa5da6b8b2607
1tiagofr <tiagfox06@gmail.com>
2025-07-12 18:31:47 -0300

    ejercicio21.java

--- ejercicio21.java
+++ ejercicio21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SueldoNetoEmpleado {
+public class ejercicio21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
