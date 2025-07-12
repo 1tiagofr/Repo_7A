89824c51f5243c5f6dfc064b96af0f2dd19186ae
1tiagofr <tiagfox06@gmail.com>
2025-07-12 18:30:35 -0300

    ejercicio16.java

--- ejercicio16.java
+++ ejercicio16.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TablaMultiplicar {
+public class ejercicio16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese un número para mostrar su tabla de multiplicar: ");
