de39890bb0bbf3cf69e633885c3e5f1f87f5f3df
1tiagofr <tiagfox06@gmail.com>
2025-07-12 18:30:49 -0300

    ejercicio17.java

--- ejercicio17.java
+++ ejercicio17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumaParesConIf {
+public class ejercicio17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese un número para sumar los pares hasta ese valor: ");
